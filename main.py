cff7de9aaae2d246c03acdb0ffd5cef01f38af77
jmckean1990 <jmckean2925@gmail.com>
2023-10-27 09:45:14 -0400

    Adding yolo README, .gitignore for yolov5 repo

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 # YOLO Setup: https://medium.com/analytics-vidhya/training-a-custom-object-detection-model-with-yolo-v5-aa9974c07088
 # YOLO Custom Training: https://docs.ultralytics.com/yolov5/tutorials/train_custom_data/#local-logging
 # YOLO Object Detection Tutorial: https://docs.ultralytics.com/datasets/detect/
-# Inference using custom trained model
+# Inference using custom trained model: https://github.com/ultralytics/yolov5/issues/7044
 
 
 import torch
 import glob
 import os
 
-model = torch.hub.load('ultralytics/yolov5', 'custom', 'yolov5/runs/train/exp4/weights/best.pt')
+model = torch.hub.load('ultralytics/yolov5', 'custom', 'best.pt')
 
 # load trained weights
 # model.load_state_dict(torch.load('yolov5/runs/train/exp4/weights/best.pt')['model'].state_dict())
