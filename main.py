c1bdb1cf55665d0a83cb7cc0e00e9119d416920b
jmckean1990 <jmckean2925@gmail.com>
2024-03-08 11:31:38 -0500

    Updated main.py

--- main.py
+++ main.py
@@ -1,120 +1,104 @@
-# YOLO Setup: https://medium.com/analytics-vidhya/training-a-custom-object-detection-model-with-yolo-v5-aa9974c07088
-# YOLO Custom Training: https://docs.ultralytics.com/yolov5/tutorials/train_custom_data/#local-logging
-# YOLO Object Detection Tutorial: https://docs.ultralytics.com/datasets/detect/
-# Inference using custom trained model: https://github.com/ultralytics/yolov5/issues/7044
-
 import torch
-import glob
 import os
-import sys
 import cv2
+import glob
+import re
+import time
+import scripts.utility as utils
+import sys
 import matplotlib
 import matplotlib.pyplot as plt
-# from easyocr_test import easyocr_test
-from scripts.utility import get_bounding_box_data, crop_from_points
+from transformers import TrOCRProcessor, VisionEncoderDecoderModel
 
 
-"""
-    This script accepts either a single image or a folder of images to run YOLOv5 object detection on.
-    argument 1: an image file name or image folder name
-    argument 2: whether to run license plate(lp) detection or character detection(char)
-        options: lp, char
-"""
-
-if len(sys.argv) < 3:
-    print("Error. Supply image or image folder and a detection mode(lp for license plate, char for character)")
-    sys.exit()
-
-image_files = ""
-weights = ""
-save_folder = r"D:\v2x-11-30-data\characters"
-
-# is the input a file or directory?
-if os.path.isdir(sys.argv[1]):
-    image_files = glob.glob(os.path.join(sys.argv[1], '*.*'))
-else:
-    image_files = [sys.argv[1]]
-
-# multiples = []
-# with open("multiple_images.txt", "r") as file:
-#     for img in file.readlines():
-#         multiples.append(img.strip())
-
-# current_crops = os.listdir(save_folder)
-# current = current_crops + multiples
-
-# print(f"num total: {len(image_files)}")
-# print(f"num current: {len(current)}")
-# new_images = []
-# for img in image_files:
-#     if img.split("\\")[-1].split(".")[0] + ".png" not in current:
-#         new_images.append(img)
-
-# print(f"new: {len(new_images)}")
-
-
-# load weights
-if sys.argv[2] == "lp":
-    weights = os.path.join("best_weights", "v-lp-detect-best.pt")
-elif sys.argv[2] == "char":
-    weights = os.path.join("best_weights", "x-char-detect-best-2.pt")
-else:
-    print("Error. Incorrect detection mode specified.")
-    sys.exit()
-
-
-model = torch.hub.load('ultralytics/yolov5', 'custom', weights)
-
-# matplotlib.use('TkAgg')
-model.eval()    
-
-batch_size = 200
-num_images = len(image_files)
-batches = []
-no_plate = 0
-
-for i in range(0, num_images, batch_size):
-    if (i + batch_size) > num_images:
-        batches.append(image_files[i:])
-    else:
-        batches.append(image_files[i:(i+batch_size)])
-
-batch_num = 0
-
-batches = [image_files[:10]]
-# with open("multiple_images.txt", "w") as file, open("no_plate.txt", "w") as n_file:
-for batch in batches:
-    batch_num += 1
-
-    if (batch_num % 100) == 0:
-        print(batch_num)
-
-    results = model(batch)
-
-    for prediction, image in zip(results.xyxy, batch):
-        image_name = image.split("\\")[-1].split(".")[0] + ".png"
-        
+def get_crops(model_output, images, padding=0, model_type=None):
+    result_list = []
+    char_list = []
+    for prediction, image in zip(model_output, images):
         if prediction.numel() == 0:
-            no_plate += 1
-            # n_file.write(f"{image_name}\n")
             continue
 
         prediction = prediction.tolist()     
-        boxes = get_bounding_box_data(prediction, padding=1)
-
-        img = cv2.imread(image)
-        # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        image_name_base = image.split("\\")[-1].split(".")[0]
-        image_name = image.split("\\")[-1].split(".")[0] + ".png"
-        os.mkdir(os.path.join(save_folder, image_name_base))
-        # save_path = os.path.join(save_folder, image_name)
-
-        # if len(boxes) > 1:
-            # file.write(f"{image_name}\n")
-        #     continue
-
-        for index, box in enumerate(boxes):
+        boxes = utils.get_bounding_box_data(prediction, padding=padding)
+        for box in boxes:
             bbox, conf, klass = box
-            crop = crop_from_points(img, bbox)
-            save_path = os.path.join(save_folder, image_name_base, f"{index}-" + image_name)
-            cv2.imwrite(save_path, crop, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
+            crop = utils.crop_from_points(image, bbox)
+
+            if model_type == "char":
+                char_list.append(crop)
+            else:
+                result_list.append(crop)
+
+        if model_type == "char":
+            result_list.append(char_list)
+            char_list = []
+
+    return result_list
+
+
+image_path = os.path.join(r"D:\v2x-11-30-data\ALPRPlateExport11-30-23", "001015_1701090118262R05_821.jpg")
+image_paths = glob.glob(os.path.join(r"D:\v2x-11-30-data\ALPRPlateExport11-30-23", "*"))
+images = []
+for path in image_paths[:2]:
+    images.append(cv2.imread(path))
+
+
+lp_weights = os.path.join("best_weights", "v-lp-detect-best.pt")
+char_weights = os.path.join("best_weights", "x-char-detect-best-2.pt")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+tic = time.time()
+lp_model = torch.hub.load('ultralytics/yolov5', 'custom', lp_weights)
+char_model = torch.hub.load('ultralytics/yolov5', 'custom', char_weights)
+processor = TrOCRProcessor.from_pretrained('microsoft/trocr-large-printed')
+trocr_model = VisionEncoderDecoderModel.from_pretrained('microsoft/trocr-large-printed')
+toc = time.time()
+load_time = toc - tic
+
+matplotlib.use("TkAgg")
+
+tic = time.time()
+
+lp_model.eval()
+char_model.eval()
+lp_model.to(device)
+
+lp_results = lp_model(images)
+license_plates = get_crops(lp_results.xyxy, images, padding=0)
+
+char_model.to(device)
+characters = []
+char_results = char_model(license_plates)
+characters = get_crops(char_results.xyxy, license_plates, padding=1, model_type="char")
+
+# ocr on lp images
+pixel_values = processor(images=license_plates, return_tensors="pt").pixel_values
+pixel_values = pixel_values.to(device)
+trocr_model.to(device)
+generated_ids = trocr_model.generate(pixel_values)
+generated_ids = generated_ids.cpu()
+# generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)
+parsed_ocr_values = [re.sub('[\W_]+', '', text) for text in generated_text]
+no_spaces = ["".join(value.split(" ")) for value in generated_text]
+print(generated_text)
+print(no_spaces)
+print(parsed_ocr_values)
+
+
+# ocr on characters
+text_list = []
+for char_list in characters:
+    pixel_values = processor(images=char_list, return_tensors="pt").pixel_values
+    pixel_values = pixel_values.to(device)
+    generated_ids = trocr_model.generate(pixel_values)
+    generated_ids = generated_ids.cpu()
+    generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)
+    text_list.append(generated_text)
+
+character_predictions = ["".join(chars) for chars in text_list]
+toc = time.time()
+processing_time = toc - tic
+print(character_predictions)
+print()
+print(f"Load Time: {load_time} seconds\tProcessing Time: {processing_time} seconds.")
